555aca1474ffe6395d7b1f1a807ffd0e0644743d
Akshath Phillips <akshathphillips@icloud.com>
2021-08-28 16:45:46 -0400

    Comment

--- src/app/shared/storage-service.service.ts
+++ src/app/shared/storage-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root' // Comment - AP - No need to provider this in the module, since its root
 })
 export class StorageServiceService {
 
