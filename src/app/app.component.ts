7a956ed69fa73ece4ac5e3aed50971c3df6aa42e
Akshath Phillips <akshathphillips@icloud.com>
2021-08-30 20:00:00 -0400

    Storage service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from "./shared/storage.service";
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = 'SWE632Project';
+
+
+	constructor(private storageService: StorageService) {
+	}
+
+	onClickStore() {
+		this.storageService.store();
+	}
+
+	onClickFetch() {
+		this.storageService.fetch();
+	}
+
+	ngOnInit(): void {
+	}
 }
---
+++ src/app/shared/storage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+	providedIn: 'root' // Comment - AP - No need to provider this in the module, since its root
+})
+export class StorageService {
+
+	constructor(private httpClient: HttpClient) {
+	}
+
+	store() {
+		this.httpClient.put(
+			'https://swe632project-7fc10-default-rtdb.firebaseio.com/data.json',
+			[{'name': 'Akshath', 'text': 'Hello world!'}]
+		).subscribe(response => {
+			console.log(response);
+		})
+	}
+
+	fetch() {
+		this.httpClient.get(
+			'https://swe632project-7fc10-default-rtdb.firebaseio.com/data.json'
+		)
+			.subscribe(response => {
+				console.log(response);
+			})
+	}
+}
