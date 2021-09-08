965ed918b6ae352d392a55992ef7bee94278d57e
Akshath Phillips <akshathphillips@icloud.com>
2021-09-08 11:24:20 -0400

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
-import { HomeComponent } from './components/home/home.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent, MenuComponent, PageNotFoundComponent } from './components';
 
 @NgModule({
 	declarations: [
